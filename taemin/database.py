dc88229ca3d35d8a1ffeb6e000b4d225fd7add89
Selim Menouar <selim.menouar@rez-gif.supelec.fr>
2016-02-11 21:47:37 +0100

    Prevent crash when using mysql server

--- taemin/database.py
+++ taemin/database.py
@@ -1,5 +1,9 @@
 from peewee import *
 import datetime
+from playhouse.shortcuts import RetryOperationalError
+
+class MySQLDB(RetryOperationalError, MySQLDatabase):
+    pass
 
 class DataBase(object):
     def __init__(self, type_, name, user="", password="", host="localhost", port=3306):
@@ -15,16 +19,22 @@ class DataBase(object):
                 database = self.db
         self.basemodel = BaseModel
 
+        if self.type_ == "mysql":
+            self.random_func = fn.Rand
+        else:
+            self.random_func = fn.Random
+
     def init_db(self):
         if self.type_ == "sqlite":
             db = SqliteDatabase(self.name)
         elif self.type_ == "pgsql":
             db = PostgresqlDatabase(self.name, user=self.user, password=self.password, host=self.host)
         elif self.type_ == "mysql":
-            db = MySQLDatabase(self.name, user=self.user, passwd=self.password, host=self.host, port=self.port)
+            db = MySQLDB(self.name, user=self.user, passwd=self.password, host=self.host, port=self.port)
         else:
             db = SqliteDatabase(":memory:")
         return db
+
 
 if __name__ == "__main__":
     database = DataBase("sqlite", "test.db")
