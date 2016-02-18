bc00194cea8896fc463bb67320b985bf072c9a62
Selim Menouar <selim.menouar@rez-gif.supelec.fr>
2016-02-18 21:44:20 +0100

    Add Plugin NiceBot (get the title of a given URL)

---
+++ taemin/plugins/nicebot/plugin.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python2
+# -*- coding: utf8 -*-
+
+import re
+import requests
+from bs4 import BeautifulSoup
+
+class TaeminNiceBot(object):
+
+    def __init__(self, taemin):
+        self.mapping = [
+            (r"""(?:http[s]?://)?(?:www\.)?youtu(?:be\.com/watch\?v=|\.be/)([\w\-]+)(&(amp;)?[\w\?=]*)?""", self._get_youtube),
+            (r"""https?://\S+""", self._get_title),
+        ]
+        self.taemin = taemin
+
+    def on_pubmsg(self, serv, msg):
+        chan = msg.chan.name
+
+        for match in self.check_generator(msg.message):
+            serv.privmsg(chan, match)
+
+    def check_generator(self, text):
+        for regex, func in self.mapping:
+            match = re.search(regex, text)
+            if not match:
+                continue
+
+            res = func(match, text)
+            if res:
+                yield res
+                break
+
+    def _get_title(self, match, text):
+        try:
+            html = requests.get(text)
+        except requests.RequestException:
+            return None
+
+        try:
+            html = html.text
+        except TypeError:
+            return None
+
+        html = BeautifulSoup(html, 'html.parser')
+
+        title = html.find("title")
+        if not title:
+            return None
+
+        return title.string.strip().encode("utf-8")
+
+    def _get_youtube(self, match, text):
+        try:
+            html = requests.get(text).text
+        except requests.RequestException:
+            return None
+
+        html = BeautifulSoup(html, 'html.parser')
+
+        user = self.get_youtube_user(html) or ""
+        title = self.get_youtube_title(html)
+        if not title:
+            return None
+
+        return "%s: %s" % (user, title)
+
+    def get_youtube_title(self, html):
+        title = html.find(id="eow-title")
+        if not title:
+            return None
+
+        return title.string.strip().encode("utf-8")
+
+    def get_youtube_user(self, html):
+        user = html.select("div.yt-user-info a.yt-uix-sessionlink")
+        if not user:
+            return None
+
+        return user[0].string.strip().encode("utf-8")
+
+def main():
+    nice = TaeminNiceBot(None)
+    for match in nice.check_generator("https://www.youtube.com/watch?v=0rtV5esQT6I"):
+        print match
+
+if __name__ == "__main__":
+    main()
+
