bbc125f62bc434aacad3db0ca1548d0241a3973e
Selim Menouar <selim.menouar@rez-gif.supelec.fr>
2016-03-02 21:34:40 +0100

    Add a synonyme plugin
    User !synonymes word to get the synonymes of the word
    
    Only work with french work

---
+++ taemin/plugins/synonymes/plugin.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2
+# -*- coding: utf8 -*-
+
+from synonymes import Synonymes
+from taemin import plugin
+
+class TaeminSynonymes(plugin.TaeminPlugin):
+    helper = {"synonymes": "Cherche les synonymes d'un mot"}
+
+    def on_pubmsg(self, msg):
+        if msg.key not in self.helper:
+            return
+
+        chan = msg.chan.name
+
+        synonymes = Synonymes.off(msg.value)
+
+        if synonymes:
+            self.privmsg(chan, "Synonymes de %s: %s" % (msg.value, ", ".join(synonymes)))
+            return
+
+        self.privmsg(chan, "Pas trouvé de synonymes...")
+
---
+++ taemin/plugins/synonymes/synonymes.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+# -*- coding: utf8 -*-
+
+from bs4 import BeautifulSoup
+import requests
+import urllib
+
+class Synonymes(object):
+    URL = "http://www.crisco.unicaen.fr/des/synonymes"
+    SELECTOR = "#synonymes td > a"
+
+    def __init__(self, word):
+        self.word = word
+        self.synonymes = self.off(word)
+
+    @classmethod
+    def off(cls, word):
+        soup = cls._get_html(word)
+
+        return [s.getText().strip().encode("utf-8") for s in soup.select(cls.SELECTOR)]
+
+    @classmethod
+    def _get_html(cls, word):
+        try:
+            res = requests.get("%s/%s" % (cls.URL, urllib.quote(word))).text
+        except requests.RequestException:
+            return None
+
+        return BeautifulSoup(res, 'html.parser')
+
+def main():
+    print Synonymes.off("test")
+    print Synonymes.off("machin")
+    print Synonymes("mémé").synonymes
+    print Synonymes.off("méazd")
+
+if __name__ == "__main__":
+    main()
+
+
