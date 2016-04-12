5bb576208cf49f82c23685674a2004d98085bead
Selim Menouar <selim.menouar@rez-gif.supelec.fr>
2016-04-12 19:19:51 +0200

    Add language detection in the Trad plugin

--- taemin/plugins/trad/plugin.py
+++ taemin/plugins/trad/plugin.py
@@ -11,13 +11,34 @@ class TaeminTrad(plugin.TaeminPlugin):
         if msg.key != "trad":
             return
 
-        val = msg.value.split(" ", 2)
-        if len(val) < 3:
-            self.privmsg(msg.chan.name, "Utilisation : !trad langue1 langue2 mot")
-        else:
-            trad = Traduction(val[2], val[0], val[1])
-            text = trad.trad
-            if trad.romaji:
-                text += " (%s)" % trad.romaji
-            self.privmsg(msg.chan.name, text)
+        options = self.parse_option(msg.value)
+
+        trad = Traduction.translate(options["word"], options["dst"], options["src"])
+        text = "[%s->%s] %s" % (trad.src, trad.dst, trad.word)
+        if trad.romaji:
+            text += " (%s)" % trad.romaji
+
+        self.privmsg(msg.chan.name, text)
+
+    @staticmethod
+    def parse_option(option):
+        option = option.split(" ", 2)
+        options = {"src": "auto", "dst": "fr"}
+
+        if not option:
+            return options
+
+        if len(option) == 1 or option[0] not in Traduction.available_languages():
+            options["word"] = " ".join(option)
+            return options
+
+        if len(option) == 2 or option[1] not in Traduction.available_languages():
+            options["word"] = " ".join(option[1:])
+            options["dst"] = option[0]
+            return options
+
+        options["word"] = option[2]
+        options["src"] = option[0]
+        options["dst"] = option[1]
+        return options
 
--- taemin/plugins/trad/trad.py
+++ taemin/plugins/trad/trad.py
@@ -5,33 +5,80 @@ from bs4 import BeautifulSoup
 import requests
 
 class Traduction(object):
-    URL = "https://translate.google.com/"
-    def __init__(self, word, language1, language2):
-        self.word = word
-        self.language1 = language1
-        self.language2 = language2
-        self.trad = word
-        self.romaji = None
-        self.html = self.get_html()
-        self.translate()
+    _URL = "https://translate.google.com/"
+    _LANGUAGES = []
 
-    def get_html(self):
-        data = {"sl": self.language1, "tl": self.language2, "js": "n", "ie": "UTF-8", "text": self.word}
-        html = requests.post(self.URL, data=data).text
+    def __init__(self, word, src, dst, romaji=None):
+        self.word = word
+        self.src = src
+        self.dst = dst
+        self.romaji = romaji
+
+    @classmethod
+    def _get_html(cls, word, dst_language, src_language="auto"):
+        data = {"sl": src_language,
+                "tl": dst_language,
+                "js": "n",
+                "ie": "UTF-8",
+                "text": word}
+
+        html = requests.post(cls._URL, data=data).text
+
         return BeautifulSoup(html, 'html.parser')
 
-    def translate(self):
-        try:
-            self.trad = self.html.find(id="result_box").span.string.encode("utf-8")
-        except Exception:
-            return
-        try:
-            self.romaji = self.html.find(id="res-translit").string.encode("utf-8")
-        except Exception:
-            return
+    @classmethod
+    def _get_trad_from_html(cls, html):
+        trad = html.find(id="result_box")
+        if not trad or not trad.span or not trad.span.string:
+            return None
+
+        return trad.span.string.encode("utf-8")
+
+    @classmethod
+    def _get_romaji_from_html(cls, html):
+        romaji = html.find(id="res-translit")
+        if not romaji or not romaji.string:
+            return None
+
+        return romaji.string.encode("utf-8")
+
+    @classmethod
+    def _get_src_language_from_html(cls, html):
+        src_language = [value.get("value")
+                        for value in html.select("#gt-sl option[selected]")
+                        if value.get("value") != "auto"]
+        print src_language
+
+        if not src_language:
+            return None
+        return src_language[0]
+
+    @classmethod
+    def available_languages(cls):
+        if not cls._LANGUAGES:
+            cls._LANGUAGES = [value.get("value")
+                              for value in cls._get_html("", "").select("#gt-sl option")
+                              if value.get("value") != "separator"]
+
+        return cls._LANGUAGES
+
+    @classmethod
+    def translate(cls, word, dst_language, src_language="auto"):
+        html = cls._get_html(word, dst_language, src_language)
+
+        return cls(cls._get_trad_from_html(html),
+                   src_language,
+                   dst_language,
+                   cls._get_romaji_from_html(html))
+
+def main():
+    trad = Traduction.translate("c'est du français", "ja")
+    print(trad.word)
+    print(trad.romaji)
+    print(trad.src)
+    print(trad.dst)
+    print Traduction.available_languages()
 
 if __name__ == "__main__":
-    trad = Traduction("Tabe", "ja", "fr")
-    print(trad.romaji)
-    print(trad.trad)
+    main()
 
