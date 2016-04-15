c399816ab70f161cbf2731b68f2686800e9bed4f
Selim Menouar <selim.menouar@rez-gif.supelec.fr>
2016-04-15 22:16:56 +0200

    Add a plugin to upload image on lut.im

---
+++ taemin/plugins/shareim/plugin.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+# -*- coding: utf8 -*-
+
+from taemin import plugin
+import requests
+
+class TaeminShareIm(plugin.TaeminPlugin):
+    helper = {"share": "Upload an image on lut.im. Usage !share http://<url-img>"}
+    _URL = "https://lut.im/"
+
+    def on_pubmsg(self, msg):
+        if msg.key not in self.helper:
+            return
+
+        chan = msg.chan.name
+
+        image = self.upload_image(msg.value)
+
+        if not image:
+            self.privmsg(chan, self.helper[msg.key])
+            return
+
+        self.privmsg(chan, image)
+
+    @classmethod
+    def upload_image(cls, url):
+        data = {"lutim-file-url": url,
+                "format": "json",
+                "first-view": "0",
+                "crypt": "0",
+                "delete-day": "0"
+                }
+        try:
+            res = requests.post(cls._URL, data=data, verify=False).json
+        except requests.RequestException:
+            return None
+
+        try:
+            return "%s%s" % (cls._URL, res["msg"]["short"])
+        except KeyError:
+            return None
+
+if __name__ == "__main__":
+    print TaeminShareIm.upload_image("http://www.aardwolf.com/images/mushclient-aardwolf-336.jpg")
