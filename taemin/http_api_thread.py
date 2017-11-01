4b4c6371408570e67da3996e7ac4abf0fc584f3c
Hugo Shaka <hugo@shaka.xyz>
2017-11-01 17:32:23 +0100

    endpoint loading

--- taemin/http_api_thread.py
+++ taemin/http_api_thread.py
@@ -12,12 +12,18 @@ class HttpApiThread(Thread):
         self.app = Flask(__name__)
 
     def update_app(self):
-        """Generates the routes"""
+        for endpoint in endpoints:
+            complete_route = endpoint.generate_route()
+            endpoint.callback = self.app.route(complete_route, endpoint.methods)(endpoint.callback)
         pass
+
+    def set_endpoints(self, endpoints):
+        self.endpoints = endpoints
 
     def run(self):
         self._continue = True
         """Some gevent black magic to expose the app""""
+        self.app.run()
 
     def stop(self):
         self._continue = False
